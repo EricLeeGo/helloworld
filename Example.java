17b55bfbf463a2858dc0126ce595225293cff002
EricLeeGo <56544327+EricLeeGo@users.noreply.github.com>
2019-10-14 22:24:32 +0800

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,9 @@
+package headfirst.designpatterns.observer.simple;
+
+public class Example{
+	public static void main(String[] args){
+		SimpleSubject simpleSubject = new SimpleSubject();
+		SimpleObserver simpleObserver = new SimpleObserver(simpleSubject);
+		simpleSubject.setValue(80);
+	}
+}
